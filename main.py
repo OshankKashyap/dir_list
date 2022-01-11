63043bb14ed99b6880974a77a7277ec79c46838e
Oshank <oshankashyap@protonmail.com>
2022-01-11 11:13:44 +0530

    added functionality for the user to enter the path manually and a check for the path if the path is valid or not

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 # Main module for the directory listing program
 
 import os
+import sys
 
 
 class Main:
-    def __init__(self):
-        self.get_curr_path()
-
-    def get_curr_path(self) -> None:
-        # Method to get the current path and store it in self.curr_path
-        # self.path = os.getcwd()
-        self.path = os.getcwd()
-
     def check_hidden(self, file: str) -> bool:
         # Check if the provided file is hidden or not
         # only for UNIX systems
@@ -21,14 +14,14 @@ class Main:
         else:
             return True
 
-    def show_content(self) -> None:
-        # Show all the content present in the current path
-
+    def show_files(self, path) -> None:
+        # Show all the content present in current path
         number = 1
-        contents = os.listdir(self.path)
+        contents = os.listdir(path)
+
         # perform a check for hidden files and only show the un-hidden files and directories
         for content in contents:
-            content = os.path.join(self.path, content)
+            content = os.path.join(path, content)
             if (
                 os.path.isfile(content) or os.path.isdir(content)
             ) and self.check_hidden(os.path.basename(content)):
@@ -36,5 +29,11 @@ class Main:
                 number += 1
 
 
+# get the path and check if it is valid or not
+path = input("Enter Path -> ")
+if not os.path.exists(path):
+    print(f"{path}: does not exist")
+    sys.exit()
+
 main = Main()
-main.show_content()
+main.show_files(path)
